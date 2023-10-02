e6dcafa9781d6aa0b4359f9e255955ceb8b6fc86
tommylay1902 <tommylay.c@gmail.com>
2023-10-01 21:47:48 -0700

    leetcode number 33

--- reverse-linked-list/reverseLinkedList.go
+++ reverse-linked-list/reverseLinkedList.go
@@ -1,3 +1,15 @@
-func reverseList() {
 
+
+func reverseList(l *ListNode) *ListNode {
+	var prev *ListNode
+	prev = nil
+	curr = l
+	for curr != nil {
+		temp := curr.next
+		curr.next = prev
+		prev = curr
+		curr = temp
+	}
+
+	return prev
 }
---
+++ search-in-rotated-sorted-array/searchInRotatedSortedArray.go
@@ -0,0 +1,6 @@
+package searchinrotatedsortedarray
+
+// [4,5,6,7,0,1,2]
+func search(nums []int, target int) int {
+
+}
